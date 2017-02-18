4a5484f345fe3a4be8acc70d0f23c1f896f2a713
Nathaniel Meyer <nathanielimeyer@gmail.com>
2017-02-18 13:47:56 -0800

    Add second form to html and js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   var nameInput = "";
+  $("#nameForm").show();
+
     $("#nameForm").submit(function(event){
       nameInput = $("input#name").val();
       if (!nameInput) {
@@ -9,8 +11,22 @@ $(document).ready(function(){
         $("#nameOutput").hide();
         $("#nameForm").hide();
         $(".echoName").text(nameInput);
-        $(".suggestedTrack").show();
+        $("#question1").show();
       }
       event.preventDefault();
     });
+
+    $("#question1").submit(function(event){
+      var answer1 = $("#answer1").val();
+      $("#question1").hide();
+      $("#question2").show();
+      $(".companyType").text(answer1);
+      $(".suggestedTrack").show();
+      event.preventDefault();
+    });
+
+// Put in last question form
+//     $(".suggestedTrack").show();
+
+
 });
